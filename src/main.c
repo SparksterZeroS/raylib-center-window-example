33ed921c44e246187ff923ca60bfa59bfc022931
Anthony I. Jackson Jr. <contact@anthonyijacksonjr.xyz>
2026-02-05 16:09:37 -0500

    Major update.
    First, the most notable change. A random object now flies around the screen to show that window and frame buffer resizing works.
    
    Speaking of frame buffers, I also added that as well. It resizes with the window.
    
    I also made the resize functionality monitor/display agnostic. So now, depending on which monitor the window is located on, it will now do resizing actions relative to said monitor.
    
    Other than that, I added a few compilation flags to show all warnings to ensure everything is well.

--- src/main.c
+++ src/main.c
@@ -1,43 +1,123 @@
 #include <raylib.h>
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
-
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 360
+#define PLAYER_SPEED_MIN 180
+#define PLAYER_SPEED_MAX 300
+#define PLAYER_SIZE 32
 
 #define MAX(a, b) ((a)>(b)? (a) : (b))
 #define MIN(a, b) ((a)<(b)? (a) : (b))
 
-#define PRIMARY_MONITOR 0
 
-/**
- * Positions the window in the center of the specified monitor.
- * 
- * @param monitor The monitor to center the window at.
- */
 void CenterWindow(int monitor);
+int GetMaxWindowScale(int width, int height, int monitor);
 int HandleWindowResize(int monitor, int currentScale, int maxScale);
+
+
 
 int main(void) {
     SetTraceLogLevel(LOG_DEBUG);
-    SetConfigFlags(FLAG_WINDOW_HIDDEN);
+    SetConfigFlags(FLAG_WINDOW_HIDDEN );
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Test Window");
-    CenterWindow(PRIMARY_MONITOR);
+    CenterWindow(0);
     ClearWindowState(FLAG_WINDOW_HIDDEN);
 
+    int currentWindowMonitor = GetCurrentMonitor();
+    int maxWindowScale = GetMaxWindowScale(WINDOW_WIDTH, WINDOW_HEIGHT, currentWindowMonitor);
 
-    const int maxScale = MIN(GetMonitorWidth(PRIMARY_MONITOR) / WINDOW_WIDTH, GetMonitorHeight(PRIMARY_MONITOR) / WINDOW_HEIGHT) + 1;
-    int currentScale = 1;
+    int currentWindowScale = 1;
+
+    RenderTexture2D frameBuffer = LoadRenderTexture(WINDOW_WIDTH, WINDOW_HEIGHT);
+    float frameBufferScale = 1.0f;
+
+    struct {
+        Rectangle rect;
+        Color color;
+        int speed;
+        float angle;
+        int directionX;
+        int directionY;
+    } player;
+
+    player.rect = (Rectangle){
+        .x = GetRandomValue(0, WINDOW_WIDTH),
+        .y = GetRandomValue(0, WINDOW_HEIGHT),
+        .width = PLAYER_SIZE,
+        .height = PLAYER_SIZE
+    };
+
+    player.speed = 200.0f;
+    player.angle = 0.0f;
+    player.directionX = 1;
+    player.directionY = 1;
+    player.color = GREEN;
 
     while (!WindowShouldClose()) {
         if (IsKeyPressed(KEY_F4)) {
-            currentScale = HandleWindowResize(PRIMARY_MONITOR, currentScale, maxScale);
+            currentWindowScale = HandleWindowResize(currentWindowMonitor, currentWindowScale, maxWindowScale);
         }
 
-        BeginDrawing();
+        frameBufferScale = MIN((float)GetScreenWidth()/WINDOW_WIDTH, (float)GetScreenHeight()/WINDOW_HEIGHT);
+;
+        if (currentWindowMonitor != GetCurrentMonitor()) {
+            currentWindowMonitor = GetCurrentMonitor();
+            maxWindowScale = GetMaxWindowScale(WINDOW_WIDTH, WINDOW_HEIGHT, currentWindowMonitor);
+        }
 
+        if (player.rect.x < 0) {
+            player.directionX = 1;
+            player.speed = GetRandomValue(PLAYER_SPEED_MIN, PLAYER_SPEED_MAX);
+        }
+
+        if ((player.rect.x + player.rect.width) > WINDOW_WIDTH) {
+            player.directionX = -1;
+            player.speed = GetRandomValue(PLAYER_SPEED_MIN, PLAYER_SPEED_MAX);
+        }
+
+        if (player.rect.y < 0) {
+            player.directionY = 1;
+            player.speed = GetRandomValue(PLAYER_SPEED_MIN, PLAYER_SPEED_MAX);
+        }
+
+        if ((player.rect.y + player.rect.height) > WINDOW_HEIGHT) {
+            player.directionY = -1;
+            player.speed = GetRandomValue(PLAYER_SPEED_MIN, PLAYER_SPEED_MAX);
+        }
+
+        player.rect.x += (float)(player.speed * player.directionX) * GetFrameTime();
+        player.rect.y += (float)(player.speed * player.directionY) * GetFrameTime();
+        player.angle += ((float)player.speed * 2.0f) * GetFrameTime();
+        BeginTextureMode(frameBuffer);
+        ClearBackground(ORANGE);
+        DrawRectanglePro(player.rect, (Vector2){ .x = player.rect.width / 2.0f, .y = player.rect.height / 2.0f }, player.angle, player.color);
+        EndTextureMode();
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        DrawTexturePro(
+            frameBuffer.texture,
+            (Rectangle){
+                .x = 0.0f,
+                .y = 0.0f,
+                .width = (float)frameBuffer.texture.width,
+                .height = (float)-frameBuffer.texture.height
+            },
+            (Rectangle){
+                .x = (GetScreenWidth() - ((float)WINDOW_WIDTH*frameBufferScale))*0.5f,
+                .y = (GetScreenHeight() - ((float)WINDOW_HEIGHT*frameBufferScale))*0.5f,
+                .width = (float)WINDOW_WIDTH * frameBufferScale,
+                .height = (float)WINDOW_HEIGHT * frameBufferScale
+            },
+            (Vector2){ .x = 0.0f, .y = 0.0f },
+            0.0f,
+            WHITE
+        );
         EndDrawing();
     }
 
+    UnloadRenderTexture(frameBuffer);
     CloseWindow();
 }
 
@@ -54,10 +134,19 @@ void CenterWindow(int monitor) {
     SetWindowPosition((int)windowPosition.x, (int)windowPosition.y);
 }
 
+int GetMaxWindowScale(int width, int height, int monitor) {
+    int monitorWidth = GetMonitorWidth(monitor);
+    int monitorHeight = GetMonitorHeight(monitor);
+    int ratioX = monitorWidth / width;
+    int ratioY = monitorHeight / height;
+
+    return ((ratioX)<(ratioY)? (ratioX) : (ratioY)); //Basically the min function.
+}
+
 int HandleWindowResize(int monitor, int currentScale, int maxScale) {
     int newScale = currentScale + 1;
-    
-    if (currentScale != maxScale) {
+
+    if (newScale != maxScale) {
         int newWindowWidth = WINDOW_WIDTH * newScale;
         int newWindowHeight = WINDOW_HEIGHT * newScale;
 
@@ -67,18 +156,16 @@ int HandleWindowResize(int monitor, int currentScale, int maxScale) {
         ClearWindowState(FLAG_WINDOW_HIDDEN);
     }
 
-    if (currentScale == maxScale) {
-        TraceLog(LOG_DEBUG, "Fullscreen!");
+    if (newScale == maxScale - 1) {
         ToggleFullscreen();
     }
 
-    if (currentScale > maxScale) {
-        TraceLog(LOG_DEBUG, "Windowed!");
+    if (newScale == maxScale) {
+        SetWindowState(FLAG_WINDOW_HIDDEN);
 
         ToggleFullscreen();
         SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-        SetWindowState(FLAG_WINDOW_HIDDEN);
         CenterWindow(monitor);
         ClearWindowState(FLAG_WINDOW_HIDDEN);
         return 1;
